652bc4c1ce95366b6120434e6b3bc124be92afea
dadadcko <33391945+dadadcko@users.noreply.github.com>
2018-04-05 16:41:57 +0200

    added ENUM
    added attributes to Person and created ENUM gender

--- Homework/hw1/DanoS/teacher/src/main/java/Person.java
+++ Homework/hw1/DanoS/teacher/src/main/java/Person.java
@@ -1,5 +1,8 @@
 public class Person {
     private String firstName;
+    private String lastName;
+    private int age;
+    private Gender gender;
 
     public String getFirstName() {
         return firstName;
@@ -9,10 +12,38 @@ public class Person {
         this.firstName = firstName;
     }
 
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
     @Override
     public String toString() {
         return "Person{" +
                 "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +
                 '}';
     }
+
 }
