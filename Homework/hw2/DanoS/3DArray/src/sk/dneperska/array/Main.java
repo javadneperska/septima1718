9d23af197056224aaade0883d7899220d3312d34
dadadcko <33391945+dadadcko@users.noreply.github.com>
2018-04-24 17:33:30 +0200

    project in main
    finished project. Yet located in main() method

--- Homework/hw2/DanoS/3DArray/src/sk/dneperska/array/Main.java
+++ Homework/hw2/DanoS/3DArray/src/sk/dneperska/array/Main.java
@@ -1,8 +1,32 @@
 package sk.dneperska.array;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
 
+
+    private static int [][][] numberArray = new int[3][3][3];
+    protected static int sum = 0 ;
+    protected static long multiplication = 1 ;
+    private  static Random rn = new Random();
+
     public static void main(String[] args) {
-	// write your code here
+        for (int i = 0; i < numberArray.length; i++) {
+            for (int j = 0; j < numberArray[i].length ; j++) {
+                for (int k = 0; k < numberArray[i][j].length; k++) {
+                    int rnd= rn.nextInt(15) + 1;
+                    numberArray[i][j][k]= rnd ;
+                    sum += numberArray[i][j][k];
+                    multiplication *= numberArray[i][j][k];
+                }
+
+            }
+        }
+
+
+        System.out.println("Random numbers generated : " +Arrays.deepToString(numberArray));
+        System.out.println("Summary : " +sum);
+        System.out.println("Multiplication : " + multiplication);
     }
 }
