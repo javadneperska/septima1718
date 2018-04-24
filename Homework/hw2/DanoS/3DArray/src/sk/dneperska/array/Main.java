1a934b6c6dbd9507472bc2004678b7ee6fb3a852
dadadcko <33391945+dadadcko@users.noreply.github.com>
2018-04-24 17:55:42 +0200

    finished
    finished version with separated methods for each action, included random generation, sum & multiplication and printData method

--- Homework/hw2/DanoS/3DArray/src/sk/dneperska/array/Main.java
+++ Homework/hw2/DanoS/3DArray/src/sk/dneperska/array/Main.java
@@ -7,26 +7,56 @@ public class Main {
 
 
     private static int [][][] numberArray = new int[3][3][3];
-    protected static int sum = 0 ;
-    protected static long multiplication = 1 ;
+    private static int sum = 0 ;
+    private static long multip = 1 ;
     private  static Random rn = new Random();
 
     public static void main(String[] args) {
+        arrayFill();
+        sum();
+        multiplication();
+        printData();
+    }
+
+    private static void printData() {
+        System.out.println("Random numbers generated : " +Arrays.deepToString(numberArray));
+        System.out.println("Summary : " +sum);
+        System.out.println("Multiplication : " + multip);
+    }
+
+    private static long multiplication() {
+        for (int i = 0; i < numberArray.length; i++) {
+            for (int j = 0; j < numberArray[i].length ; j++) {
+                for (int k = 0; k < numberArray[i][j].length; k++) {
+                    multip *= numberArray[i][j][k];
+                }
+
+            }
+        }
+        return multip;
+    }
+
+    private static int sum() {
+        for (int i = 0; i < numberArray.length; i++) {
+            for (int j = 0; j < numberArray[i].length ; j++) {
+                for (int k = 0; k < numberArray[i][j].length; k++) {
+                    sum += numberArray[i][j][k];
+                }
+
+            }
+        }
+        return sum;
+    }
+
+    private static void arrayFill(){
         for (int i = 0; i < numberArray.length; i++) {
             for (int j = 0; j < numberArray[i].length ; j++) {
                 for (int k = 0; k < numberArray[i][j].length; k++) {
                     int rnd= rn.nextInt(15) + 1;
                     numberArray[i][j][k]= rnd ;
-                    sum += numberArray[i][j][k];
-                    multiplication *= numberArray[i][j][k];
                 }
 
             }
         }
-
-
-        System.out.println("Random numbers generated : " +Arrays.deepToString(numberArray));
-        System.out.println("Summary : " +sum);
-        System.out.println("Multiplication : " + multiplication);
     }
 }
