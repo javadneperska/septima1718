668054f4f47645d5f150ad990c3e97ee18914c95
dadadcko <33391945+dadadcko@users.noreply.github.com>
2018-05-14 21:08:10 +0200

    added fileReader class
    Co-Authored-By: Anastasiia <anasttasiia@users.noreply.github.com>

---
+++ Homework/hw3/Dano S & Nasťa/Objects/src/sk/dneperska/objects/Buffer.java
@@ -0,0 +1,44 @@
+package sk.dneperska.objects;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Buffer {
+    private String path = "C:\\Users\\Peter\\Desktop\\File.txt";
+    FileReader fr;
+    public String [][] myArray = new String[4][4];
+
+
+    public void read() {
+        try {
+            Scanner sc = new Scanner(new BufferedReader(new FileReader(System.getProperty("user.home") +"\\Documents\\GitHub\\septima1718\\Homework\\hw3\\Dano S & Nasťa\\Objects\\input.txt")));
+            while(sc.hasNextLine()) {
+                for (int i=0; i<myArray.length; i++) {
+                    String[] line = sc.nextLine().trim().split(",");
+                    for (int j=0; j<line.length; j++) {
+                        myArray[i][j] = line[j];
+                    }
+                    for(int k=0; k<myArray.length; k++){
+                        for(int l=0; l<myArray.length; l++){
+                            if(myArray[k][l]== null){
+
+                                myArray[k][l]="0";
+                            }
+
+
+                        }
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(Arrays.deepToString(myArray));
+    }
+}
