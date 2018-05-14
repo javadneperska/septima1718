aacf6e98ac728f879c7666e04cee87b120c1af31
dadadcko <33391945+dadadcko@users.noreply.github.com>
2018-05-14 22:43:22 +0200

    added separator in main()
    Co-Authored-By: Anastasiia <anasttasiia@users.noreply.github.com>

--- Homework/hw3/Dano S & Nasťa/Objects/src/sk/dneperska/objects/Buffer.java
+++ Homework/hw3/Dano S & Nasťa/Objects/src/sk/dneperska/objects/Buffer.java
@@ -8,20 +8,20 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Buffer {
-    public String [][] myArray = new String[4][4];
+    public String [][] readArray = new String[4][4];
     public void read() {
         try {
             Scanner sc = new Scanner(new BufferedReader(new FileReader(System.getProperty("user.home") +"\\Documents\\GitHub\\septima1718\\Homework\\hw3\\Dano S & Nasťa\\Objects\\input.txt")));
             while(sc.hasNextLine()) {
-                for (int i=0; i<myArray.length; i++) {
+                for (int i = 0; i< readArray.length; i++) {
                     String[] line = sc.nextLine().trim().split(",");
                     for (int j=0; j<line.length; j++) {
-                        myArray[i][j] = line[j];
+                        readArray[i][j] = line[j];
                     }
-                    for(int k=0; k<myArray.length; k++){
-                        for(int l=0; l<myArray.length; l++){
-                            if(myArray[k][l]== null){
-                                myArray[k][l]="0";
+                    for(int k = 0; k< readArray.length; k++){
+                        for(int l = 0; l< readArray.length; l++){
+                            if(readArray[k][l]== null){
+                                readArray[k][l]="0";
                             }
                         }
                     }
@@ -32,6 +32,6 @@ public class Buffer {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println(Arrays.deepToString(myArray));
+        System.out.println(Arrays.deepToString(readArray));
     }
 }
--- Homework/hw3/Dano S & Nasťa/Objects/src/sk/dneperska/objects/Main.java
+++ Homework/hw3/Dano S & Nasťa/Objects/src/sk/dneperska/objects/Main.java
@@ -3,14 +3,33 @@ package sk.dneperska.objects;
 public class Main {
 
     public static void main(String[] args) {
-
         Buffer a = new Buffer();
         a.read();
-        double lol = Double.parseDouble(a.myArray[0][1]);
-        Square b = new Square(lol);
-        b.printData();
+        for (int i = 0;i<a.readArray.length;i++){
+            if (a.readArray[i][0].equals("square")){
+                double lol = Double.parseDouble(a.readArray[i][1]);
+                Square geometricShape = new Square(lol);
+                geometricShape.printData();
+            }
+            if (a.readArray[i][0].equals("rectangle")){
+                double lol = Double.parseDouble(a.readArray[i][1]);
+                double lol2 = Double.parseDouble(a.readArray[i][2]);
+                Rectangle geometricShape = new Rectangle(lol,lol2);
+                geometricShape.printData();
+            }
+            if (a.readArray[i][0].equals("triangle")){
+                double lol = Double.parseDouble(a.readArray[i][1]);
+                double lol2 =Double.parseDouble(a.readArray[i][2]);
+                double lol3 = Double.parseDouble(a.readArray[i][3]);
+                Triangle geometricShape = new Triangle(lol,lol2,lol3);
+                geometricShape.printData();
+            }
+            if (a.readArray[i][0].equals("circle")){
+                double lol = Double.parseDouble(a.readArray[i][1]);
+                Circle geometricShape = new Circle(lol);
+                geometricShape.printData();
+            }
 
-
-
+        }
     }
 }
