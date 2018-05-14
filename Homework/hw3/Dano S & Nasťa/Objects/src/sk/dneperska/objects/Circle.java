e055369d1e075bdbdd291d9b36b33ccd4bf9d461
Anastasiia <anastasiiaa.25.05@gmail.com>
2018-05-14 22:24:07 +0200

    added
    Co-Authored-By: Daniel Slanina <dadadcko@users.noreply.github.com>

--- Homework/hw3/Dano S & Nasťa/Objects/src/sk/dneperska/objects/Circle.java
+++ Homework/hw3/Dano S & Nasťa/Objects/src/sk/dneperska/objects/Circle.java
@@ -1,4 +1,26 @@
 package sk.dneperska.objects;
 
 public class Circle {
+    private double size1;
+
+    public Circle(double size1) {
+        this.size1 = size1;
+
+
+    }
+
+
+    public double capacity(){
+        double result = Math.PI*size1*size1;
+        return result;
+    }
+
+
+    public double circuit(){
+        double result = 2*Math.PI*size1*size1;
+        return result;
+    }
+    public void printData(){
+        System.out.println("Circuit of the circle with size1 : " +size1  + "cm  is : " +circuit() +"cm and capacity is : " +capacity() + "cm^2");
+    }
 }
--- Homework/hw3/Dano S & Nasťa/Objects/src/sk/dneperska/objects/Triangle.java
+++ Homework/hw3/Dano S & Nasťa/Objects/src/sk/dneperska/objects/Triangle.java
@@ -1,4 +1,29 @@
 package sk.dneperska.objects;
 
 public class Triangle {
+    private double size1;
+    private double size2;
+    private double size3;
+    public Triangle(double size1,double size2, double size3) {
+        this.size1 = size1;
+        this.size2 = size2;
+        this.size3 = size3;
+
+    }
+
+
+    public double capacity(){
+        double result = Math.sqrt((circuit()/2)*((circuit()/2)-size2)*((circuit()/2)-size3)*((circuit()/2)-size3));
+        return result;
+    }
+
+
+    public double circuit(){
+        double result = size1+size2+size3;
+        return result;
+    }
+
+    public void printData(){
+        System.out.println("Circuit of the triangle with size1 : " +size1 +"cm, size2 : "+size2 + "cm and size3 : " + "cm  is : " +circuit() +"cm and capacity is : " +capacity() + "cm^2");
+    }
 }
