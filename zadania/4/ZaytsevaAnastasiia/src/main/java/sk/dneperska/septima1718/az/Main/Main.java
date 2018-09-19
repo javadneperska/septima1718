1bb52c24cccd8fe0e91617644f77eea36b25e4d8
Anastasiia <anastasiiaa.25.05@gmail.com>
2018-09-19 10:44:47 +0200

    ...

---
+++ zadania/4/ZaytsevaAnastasiia/src/main/java/sk/dneperska/septima1718/az/Main/Main.java
@@ -0,0 +1,18 @@
+package sk.dneperska.septima1718.az.Main;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+       Zakaznici Peter = new Zakaznici("Peter", "Ferencik");
+        Zakaznici Miro = new Zakaznici("Miro", "Katrak");
+        Zakaznici Ada = new Zakaznici("Ada", "Michalovitsova");
+        Zakaznici Kiko = new Zakaznici("Kiko", "Satmari");
+        System.out.println(Zakaznici.pocet);
+        // Prints "Hello, World" to the terminal window.
+
+
+
+    }
+}
