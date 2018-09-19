3eb7155c9816e7632918c1dd3cb1af80f94ac57b
kedar389 <kedar389@gmail.com>
2018-09-19 10:50:33 +0200

    tčrge

---
+++ zadania/4/SedlakRadek/Zakaznici/src/zakaznici/Zakaznici.java
@@ -0,0 +1,18 @@
+
+package zakaznici;
+
+
+public class Zakaznici {
+
+   
+    public static void main(String[] args) {
+     Zakaznik ferko=new Zakaznik ("Ferko","Horvath") ;
+     Zakaznik ernest=new Zakaznik ("Ernest","Šuhaj") ;
+     Zakaznik kubko=new Zakaznik ("Kubo","Horvath") ;
+     Zakaznik pali=new Zakaznik ("Pali","Zanicky") ;
+     
+     
+     
+    }
+    
+}
--- zadania/4/SedlakRadek/main/src/main/Main.java
+++ zadania/4/SedlakRadek/main/src/main/Main.java
@@ -16,7 +16,7 @@ public class Main {
      */
     public static void main(String[] args) {
         System.out.println("hello");
-        System.out.println("LOL");
+        System.out.println("LOL ");
     }
     
 }
