efd2e1b5fbeb8f2dc58b96bad751195e38a52584
Jakub7M <muzik.jakub@centrum.sk>
2018-09-19 10:43:06 +0200

    d
    d

---
+++ zadania/4/MuzikJakub/src/Main/java/Zakaznik.java
@@ -0,0 +1,23 @@
+
+public class Zakaznik {
+    private String meno;
+    private String priezvisko;
+    
+    public static pocetZakaznikov=0;
+
+    public String getMeno() {
+        return meno;
+    }
+
+    public void setMeno(String meno) {
+        this.meno = meno;
+    }
+
+    public String getPriezvisko() {
+        return priezvisko;
+    }
+
+    public void setPriezvisko(String priezvisko) {
+        this.priezvisko = priezvisko;
+    }
+}
