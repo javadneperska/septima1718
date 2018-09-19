fb73823f03582f39bf5f7743acc99ffc19f6c937
dadadcko <dano.slanina@gmail.com>
2018-09-19 10:33:00 +0200

    Customer counter (firstApp)

---
+++ zadania/4/SlaninaDaniel/src/main/java/sk/dneperska/septima1718/daniel/slanina/Main.java
@@ -0,0 +1,13 @@
+package sk.dneperska.septima1718.daniel.slanina;
+
+public class Main {
+
+    public static void main(String[] args){
+        Customer dano = new Customer("Daniel","Slanina");
+        Customer ferok = new Customer("Peter","Ferok");
+
+        System.out.println("Name: " + dano.getFirstName() +"\n"+"Surname: " + dano.getLastName());
+        System.out.println("Name: " + ferok.getFirstName() +"\n"+"Surname: " + ferok.getLastName());
+        System.out.println("Number of customers : " +Customer.pocet);
+    }
+}
