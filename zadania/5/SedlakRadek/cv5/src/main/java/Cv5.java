0def557edad0957ecad44b71963498f99ec3db02
kedar389 <kedar389@gmail.com>
2018-09-26 09:06:10 +0200

    ad

--- zadania/5/SedlakRadek/cv5/src/main/java/Cv5.java
+++ zadania/5/SedlakRadek/cv5/src/main/java/Cv5.java
@@ -3,19 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package main.java;
+package cv5;
 
 /**
  *
  * @author Žiak
  */
-public class JavaApplication6 {
+public class Cv5 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("kkk");
+        // TODO code application logic here
     }
     
 }
