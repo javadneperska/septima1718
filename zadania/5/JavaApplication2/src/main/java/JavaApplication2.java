6cd2b4b0d61d692c6bb95f543574fae34179c93f
apophis22 <36695431+apophis22@users.noreply.github.com>
2018-09-26 08:35:30 +0200

    zmena

---
+++ zadania/5/JavaApplication2/src/main/java/JavaApplication2.java
@@ -0,0 +1,11 @@
+package main.java;
+import java.util.Scanner;
+public class JavaApplication2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Say Hi");
+        String say = sc.next();
+        System.out.println("Hello");
+    }
+    
+}
