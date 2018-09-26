91829b7c19c1b204a85c52f16ee1f6d7a11a0188
JakubS <jakub.sejna@gmail.com>
2018-09-26 09:07:02 +0200

    :(

--- zadania/5/SejnaJakub/src 8.50.13/main/java/cv5.java
+++
@@ -1,9 +0,0 @@
-import javax.xml.bind.SchemaOutputResolver;
-
-public class FifthApp {
-    public static void main(String[]args){
-        System.out.println("I am on fifth lesson.");
-
-    }
-
-}
