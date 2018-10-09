080bc55a84a32d023fbac87cd4a368711170a93d
dadadcko <dano.slanina@gmail.com>
2018-10-09 14:43:03 +0200

    regular update

--- Homework/hw1/SlaninaDaniel/src/main/java/sk/dneperska/septima1718/daniel/slanina/Main.java
+++ Homework/hw1/SlaninaDaniel/src/main/java/sk/dneperska/septima1718/daniel/slanina/Main.java
@@ -1,33 +1,25 @@
 package sk.dneperska.septima1718.daniel.slanina;
 
 
-import java.io.*;
-import java.lang.management.ManagementFactory;
-
 public class Main {
 
 
     public static void main(String[] args) {
+        utils.getInfo();
+        System.out.println((int)utils.memory);
+        int field[] = new int[(int) utils.memory *2 +10500000];
+        for (int i = 0; i < field.length; i++) {
+            field[i]= i;
+        }
+        System.out.println(utils.memory);
+        System.out.println(utils.freeMemory/1048576);
+        System.out.println(field.length);
+        System.out.println("--------------------------------------------------------------------");
+        utils.freeMemory = 0;
+        utils.memory = 0;
+        utils.maxMemory = 0;
+        utils.getInfo();
 
-        /* Total number of processors or cores available to the JVM */
-        System.out.println("Available processors (cores): " +
-                Runtime.getRuntime().availableProcessors());
-
-        /* Total amount of free memory available to the JVM */
-        long freeMemory = Runtime.getRuntime().freeMemory();
-        System.out.println("Free memory (MegaBytes): " + freeMemory/1048576);
-
-        /* This will return Long.MAX_VALUE if there is no preset limit */
-        long maxMemory = Runtime.getRuntime().maxMemory();
-        /* Maximum amount of memory the JVM will attempt to use */
-        System.out.println("Maximum memory (MegaBytes): " +
-                (maxMemory == Long.MAX_VALUE ? "no limit" : maxMemory/1048576));
-
-        /* Total memory currently in use by the JVM */
-        long memory =  Runtime.getRuntime().totalMemory();
-        System.out.println("Total memory (MegaBytes): " +memory/1048576);
-
-        long memorySize = ((com.sun.management.OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean()).getTotalPhysicalMemorySize();
-        System.out.println("RAM Size= " + memorySize + " Bytes, what is equiv. of " + memorySize / 1048576 + " MegaBytes");
     }
+
 }
