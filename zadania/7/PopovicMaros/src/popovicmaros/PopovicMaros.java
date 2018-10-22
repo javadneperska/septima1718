733021b5343c55980aaa68359f1ce6e36d741d56
maros101 <38110571+maros101@users.noreply.github.com>
2018-10-22 12:46:11 +0200

    Summary (required)
    Description

--- zadania/7/PopovicMaros/src/popovicmaros/PopovicMaros.java
+++ zadania/7/PopovicMaros/src/popovicmaros/PopovicMaros.java
@@ -9,9 +9,10 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -20,77 +21,169 @@ import java.util.Scanner;
 public class PopovicMaros {
 
 static Scanner sc = new Scanner(System.in);
-static ArrayList kosik=new ArrayList();
+static List<String> kosik = new ArrayList<String>();
+static List<String> fileNames;
+static List<String> productNames;
+public static String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+public static String ANSI_RED_BACKGROUND = "\u001B[46m";
+public static String ANSI_RESET = "\u001B[0m";
 
+        // ********************************************* main() *********************************************
     public static void main(String[] args) {
+        menu();
+    }
+    
+    // ********************************************* menu() *********************************************
+    private static void menu() {
+        File file = new File("src//popovicmaros//");
+        String[] fileList = file.list();
+        fileNames = new ArrayList<String>();
+        productNames = new ArrayList<String>();
+        for(String name:fileList){
+               if(Pattern.matches(".*.csv", name)) {
+                    fileNames.add(name);
+                    productNames.add(name.replace("_", " ").replace(".csv", ""));
+               }
+        }
+        /*
+        for (String fileName : fileNames) {
+            System.out.println(fileName);
+        }
+        
+        for (String productName : productNames) {
+            System.out.println(productName);
+        }
+        */
      while(true){
-        System.out.println("Write component you are looking for: \n");
-        System.out.println("(1) RAM");
-        System.out.println("(2) Graphics card");
-        System.out.println("(3) Processor");
-        System.out.println("(4) Hard disk");
+         
+        System.out.println("\n\n*** MENU ****");
+        System.out.println(ANSI_GREEN_BACKGROUND + "Press number of component you are looking for: \n"+ANSI_RESET);
+        int choice = 1;
+         for (String productName : productNames) {
+             System.out.println("("+choice+") "+productName);
+             choice++;
+         }
+         
+         
+         
+        
         System.out.println("-----------------");
+        System.out.println("(-1) Show cart");
         System.out.println("(0) Buy");
         System.out.println("(any) Stop");
-        switch(sc.nextInt()){
-            case 1: getFromCsv("ram"); break;
-            case 2: getFromCsv("graphiccard"); break;
-            case 3: getFromCsv("processor"); break;
-            case 4: getFromCsv("harddisk"); break;
-            case 0: buy(kosik);
-            default: System.exit(0); break;
-        }
-     }   
-    }
-
-    private static void getFromCsv(String productName) {
+        choice = sc.nextInt();
+         System.out.println("\n\n\n");
         
+        
+        if(choice == 0){
+            buy(kosik);
+            break;
+        }else if(choice == -1){
+            showCart();
+        }else{
+            getFromCsv(choice-1);
+        }
+     }  
+    }
+       // ******************************************************************************************** 
+    
+
+        // ********************************************* getFromCsv() *********************************************
+    private static void getFromCsv(int choice) {
+        String fileName = fileNames.get(choice);
+       // System.out.println("You choose:"+choice);
+        System.out.println(ANSI_RED_BACKGROUND +productNames.get(choice)+"s"+ANSI_RESET);
         
         try {
 
-            File f = new File("src//popovicmaros//"+productName+".csv");
+            File f = new File("src//popovicmaros//"+fileName);
             BufferedReader b = new BufferedReader(new FileReader(f));
             String readLine = "";
-            ArrayList<String> list=new ArrayList<String>();
-            System.out.println(b.readLine()+"\n");
+            ArrayList<String> productInfoList = new ArrayList<String>();
+            System.out.println("-------------------------------");
+            System.out.println("\t   "+b.readLine()+"\n");
             int option = 1;
             while(null != (readLine = b.readLine())){
                 System.out.println("Option ("+option+"): "+readLine);
-                list.add(readLine);
+                productInfoList.add(productNames.get(choice)+": "+readLine);
                 option++;
             }
+            System.out.println("-------------------------------\n");
             
-            System.out.println("Press option or press (0) to get back to menu");
+            System.out.println(ANSI_GREEN_BACKGROUND + "Select option or press (0) to get back to menu"+ANSI_RESET);
             option = sc.nextInt();
             if(option != 0){
-                System.out.println(list.get(option-1));
-                kosik.add(createClass(list.get(option-1),productName));
+                System.out.println(productInfoList.get(option-1));
+                kosik.add(productInfoList.get(option-1));
             }
+            System.out.println("\n\n\n");
                  } catch (IOException e) {
                      e.printStackTrace();
                    }
     }
 
-    private static Object createClass(String get, String productName) {
-        switch(productName){
-            case "ram":
-                ram r = new ram(get.split(",")[0],get.split(",")[1],get.split(",")[2],Double.parseDouble(get.split(",")[3]));
-                return r;
-            case "graphiccard":
-                graphiccard g = new graphiccard(get.split(",")[0],get.split(",")[1], Double.parseDouble(get.split(",")[2]));
-                return g;
-        }
-        System.out.println("d,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,");
-        return null;
-    }
+// ******************************************************************************************
 
-    private static void buy(ArrayList kosik) {
-        System.out.println("Items in cart:");
-        for (Object object : kosik) {
-            System.out.println(object.toString());
+        // ********************************************* buy() *********************************************
+    private static void buy(List<String> kosik) {
+        List<Double> total = new ArrayList<Double>();
+        if(kosik.isEmpty()){
+            System.out.println(ANSI_GREEN_BACKGROUND + "\nCart empty."+ANSI_RESET);
+            menu();
         }
+        System.out.println("\n--------------------------------");
+        System.out.println(ANSI_GREEN_BACKGROUND + "Items in cart:\n"+ANSI_RESET);
+        for (String product : kosik) {
+            System.out.println(product);
+            total.add(Double.parseDouble(product.split(",")[product.split(",").length-1].replace("€","")));
+        }
+        double eur = 0;
+        for (Double e : total) {
+            eur += e;
+        }
+        eur = (Math.round((eur*100)));
+        eur = eur/100;
+        System.out.println(ANSI_GREEN_BACKGROUND + "Total: "+eur+" €"+ANSI_RESET);
+        System.out.println("--------------------------------\n");
+        System.out.println("Buy? "+ ANSI_GREEN_BACKGROUND + "(1) Yes"+ANSI_RED_BACKGROUND + " (0) No"+ANSI_RESET);
+        switch(sc.nextInt()){
+            case 1: System.out.println(ANSI_GREEN_BACKGROUND + "Bought"+ANSI_RESET); System.exit(0);break;
+            case 0: System.out.println(ANSI_RED_BACKGROUND + "Not bought"+ANSI_RESET);
+                    break;
+        }
+        menu();
+        
+    }
+        // ********************************** showCart() ********************************************************
+
+    private static void showCart() {
+        List<Double> total = new ArrayList<Double>();
+        if(kosik.isEmpty()){
+            System.out.println(ANSI_GREEN_BACKGROUND + "\nCart empty."+ANSI_RESET);
+            menu();
+        }
+        System.out.println("\n--------------------------------");
+        System.out.println(ANSI_RED_BACKGROUND +"Items in cart:\n"+ANSI_RESET);
+        for (String product : kosik) {
+            System.out.println(product);
+            total.add(Double.parseDouble(product.split(",")[product.split(",").length-1].replace("€","")));
+        }
+        double eur = 0;
+        for (Double e : total) {
+            eur += e;
+        }
+        eur = (Math.round((eur*100)));
+        eur = eur/100;
+        System.out.println(ANSI_RED_BACKGROUND +"Total: "+eur+" €" + ANSI_RESET);
+        System.out.println("--------------------------------\n");
+        System.out.println(ANSI_GREEN_BACKGROUND +"Press (0) to empty cart or press any key to return."+ANSI_RESET);
+        if("0".equals(sc.next())){
+            kosik.clear();
+            System.out.println("Cart empty."+ANSI_RESET);
+        }
+        menu();
     }
     
-    
+    // ******************************************************************************************
     
 }
