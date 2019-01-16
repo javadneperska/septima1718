9919712968996a5fa5955795bad77f27fdd62039
JakubS <jakub.sejna@gmail.com>
2019-01-16 09:27:20 +0100

    :)

---
+++ Maturita/3/SejnaJakub/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+            int[] numbers= new int[10];
+            Scanner sc=new Scanner(System.in);
+            System.out.println("Zadaj 10 cisel  ");
+            for (int i = 0; i < 10; i++) {
+                numbers[i]= sc.nextInt();
+
+            }
+            for (int b : numbers) {
+                if(b%2==0 || b%7==0) {
+                    System.out.println(b);
+                }
+    }
+}
+}
+
+
