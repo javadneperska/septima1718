14f4f3c99985799d0a2025b69d9a3a5040fb8ae8
kedar389 <kedar389@gmail.com>
2019-01-16 10:01:06 +0100

    Update JavaApplication18.java

--- Maturita/5/SedlakRadek/JavaApplication18/src/javaapplication18/JavaApplication18.java
+++ Maturita/5/SedlakRadek/JavaApplication18/src/javaapplication18/JavaApplication18.java
@@ -8,12 +8,35 @@ public class JavaApplication18 {
 
     
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("zadaj cislo od ktoreho zacat");
-     int x=sc.nextInt();
-      
-if( x% 1 == 0| x%x== 0)
-System.out.print(" it is prime number");
+      int a,b,c;
+Scanner s=new Scanner(System.in);
+System.out.println("zadaj cislo ");
+
+        a =s.nextInt();
+
+        b=1;
+
+        c=0;
+
+        while(b<= a)
+
+        {
+
+            if((a%b)==0)
+
+                c=c+1;
+
+            b++;
+
+        }
+
+        if(c==2)
+
+            System.out.println(a +" toto cislo je prvocislo");
+
+        else
+
+            System.out.println(a +" toto cislo nie je prvocislo");
     }
     
 }
