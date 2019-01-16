7c8122b2d89fea6003e3d031522f4c05a0cf780f
JakubS <jakub.sejna@gmail.com>
2019-01-16 10:28:21 +0100

    nice

---
+++ Maturita/6/SejnaRadoslav/src/com/company/Main.java
@@ -0,0 +1,41 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String season;
+        System.out.println("Enter any month(1 to 12)");
+        Scanner s = new Scanner(System.in);
+        int entry = s.nextInt();
+        switch (entry)
+        {
+            case 12:
+            case 1:
+            case 2:
+                season = "Zima";
+                break;
+            case 3:
+            case 4:
+            case 5:
+                season = "Jar";
+                break;
+            case 6:
+            case 7:
+            case 8:
+                season = "Leto";
+                break;
+            case 9:
+            case 10:
+            case 11:
+                season = "Jesen";
+                break;
+            default:
+                season = "Neexistuje";
+        }
+        System.out.println("Mesiac je  " + season + ".");
+    }
+}
+
+
