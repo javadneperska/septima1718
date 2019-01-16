cc28603ad68a327687ad60f25160c3d8776415ef
kiramtas99 <36226103+kiramtas99@users.noreply.github.com>
2019-01-16 10:01:55 +0100

    2 BugFix

--- Maturita/2/SlaninaDaniel/src/com/google/com/Main.java
+++ Maturita/2/SlaninaDaniel/src/com/google/com/Main.java
@@ -5,25 +5,27 @@ import java.util.Scanner;
 
 public class Main {
     private static Scanner sc = new Scanner(System.in);
+    private static int count;
+    private static int j = 0;
 
     public static void main(String[] args) {
         System.out.println("first number: ");
         int a = sc.nextInt();
         System.out.println("second number: ");
         int b = sc.nextInt();
-        int[] numbers = new int[(b-a+1)/2];
-
-        for (int i = 0; i <= numbers.length; i++) {
-            if (a > b) break;
-
-            if ( (a % 2) == 0) {
-                numbers[i] = a;
-                a++;
+        for (int i = a; i <= b; i++) {
+            if ((i % 2) == 0) {
+                count++;
+            }
+        }
+        int[] numbers = new int[count];
+        for (int i = 0; i < b; i++) {
+            if ((a % 2) == 0) {
+                numbers[j] = a;
+                j++;
             }
             a++;
-
         }
-
         System.out.println(Arrays.toString(numbers));
 
     }
