a86d14ab0055dd368742c2ee23ad2a989fcce110
Anastasiia <anastasiiaa.25.05@gmail.com>
2019-01-17 16:27:45 +0100

    ...

--- Maturita/3/ZaytsevaAnastasiia/src/com/company/Main.java
+++ Maturita/3/ZaytsevaAnastasiia/src/com/company/Main.java
@@ -7,14 +7,14 @@ public class Main {
     public static void main(String[] args) {
         int i=0;
 
-        float arr[]=new float[10];
+        int arr[]=new int[10];
        Scanner sc = new Scanner(System.in);
         for(i=0;i<arr.length;++i)
-            arr[i]=sc.nextFloat();
+            arr[i]=sc.nextInt();
 
-        System.out.println("\n\nDivided by 2 & 7: ");
+        System.out.println("\n\nDivided by 2 or 7: ");
         for (int j=0; j<arr.length; j++) {
-            if (arr[j]%2==0 || arr[j]%7==0) System.out.print(j +", ");
+            if (arr[j]%2==0 || arr[j]%7==0) System.out.print(arr[j] +"  ");
         }
         System.out.println("\n");
 
