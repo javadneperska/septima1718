547c4ca538766572a0b32cf7408da9fec4d90632
Anastasiia <anastasiiaa.25.05@gmail.com>
2019-01-23 09:38:05 +0100

    ...

---
+++ Maturita/9/ZaytsevaAnastasiia/src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+    static void g(int n){
+        if(n>1){
+            System.out.println("*");
+            String spaces = "";
+            for(int i=0;i<n-2;i++){
+                System.out.println("*"+spaces+"*");
+                spaces+=" ";
+            }
+            for(int i=0;i<n;i++) System.out.print("*");
+        }
+        else System.out.println("*");
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        g(n);
+    }
+}
