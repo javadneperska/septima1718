53458d26fbd65ef5d33805cec20125982be8da8d
Peter <peto.ferencik@gmail.com>
2019-01-23 08:23:16 +0100

    Project7

---
+++ Maturita/7/FerencikPeter/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String word = sc.next();
+        int length = word.length();
+        if(length < 4){
+
+            System.out.println("The word is not long enough!");
+        }
+        else{
+            System.out.println(word);
+            String reverse = new StringBuffer(word).reverse().toString();
+            System.out.println(reverse);
+            System.out.println("Number of letters is:"+ length);
+        }
+
+    }
+}
