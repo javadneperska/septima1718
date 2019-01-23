81f1bd5e1cac380728c89c38147757e8006de7c6
maros101 <38110571+maros101@users.noreply.github.com>
2019-01-23 08:28:43 +0100

    Add files via upload

---
+++ Maturita/7/PopovicMaros/src/main/java/sk/dneperska/septima1718/popovic/popovicmaros/NewMain.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sk.dneperska.septima1718.popovic.popovicmaros;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Žiak
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = "";
+        while(s.length()<5){
+            System.out.println("Zadaj slovo (min 5 znakov)");
+            s = sc.nextLine();
+        }
+        for (int i = s.length()-1; i >= 0; i--) {
+           System.out.print(s.charAt(i)); 
+        }
+        System.out.println("\nPocet pismen je "+s.length());
+        
+    }
+    
+}
