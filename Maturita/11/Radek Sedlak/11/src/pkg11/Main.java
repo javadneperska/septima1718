1d5d3e130f959fbf41342bd3c48a5e46ecb44cf4
kedar389 <36226114+kedar389@users.noreply.github.com>
2019-01-30 10:25:18 +0100

    d

--- Maturita/11/Radek Sedlak/11/src/pkg11/Main.java
+++ Maturita/11/Radek Sedlak/11/src/pkg11/Main.java
@@ -5,6 +5,8 @@
  */
 package pkg11;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Žiak
@@ -15,7 +17,30 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+     Scanner sc = new Scanner(System.in);
+        int i;
+        int j;
+        System.out.println("kolko schodov?");
+        int g=sc.nextInt();
+        System.out.println("aky dlhý schod");
+        int h=sc.nextInt();
+        String a = "     ";
+       
+          
+    for (i = 1; i <= g; i++) {
+
+    for (j = 1; j < i; j++) {
+                
+        System.out.print(a);
+    }
+        for (j = 1; j <= h; j++) {
+    System.out.print("*");
+    }
+    System.out.println("");
+    
+    
+    }
+        
+    }
     }
     
-}
