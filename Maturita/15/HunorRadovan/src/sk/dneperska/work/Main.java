f9a2dbbb746b26d7ca5d371d137baeffeba555fe
apophis22 <36695431+apophis22@users.noreply.github.com>
2019-02-13 08:49:42 +0100

    15

---
+++ Maturita/15/HunorRadovan/src/sk/dneperska/work/Main.java
@@ -0,0 +1,29 @@
+package sk.dneperska.work;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	int input = 0;
+	try{
+	    input = sc.nextInt();
+	    output(input);
+    }catch(Exception e){
+	    System.exit(0);
+    }
+    }
+    static void output(int input){
+        String check[] = Integer.toString(input).split("");
+        int control = 0;
+        int control2 = 0;
+        for(int x = 0;x<check.length;x++){
+            control2 = Integer.parseInt(check[x]);
+            if(control2 > control){
+                control = control2;
+            }
+        }
+        System.out.println(control);
+    }
+}
