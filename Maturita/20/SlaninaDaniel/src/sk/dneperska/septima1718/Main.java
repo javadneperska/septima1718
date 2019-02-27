57335197d0aa478f1cee6a64ddda56a2e91c25de
kiramtas99 <36226103+kiramtas99@users.noreply.github.com>
2019-02-27 09:05:26 +0100

    update

--- Maturita/20/SlaninaDaniel/src/sk/dneperska/septima1718/Main.java
+++ Maturita/20/SlaninaDaniel/src/sk/dneperska/septima1718/Main.java
@@ -4,14 +4,22 @@ import java.util.Scanner;
 
 public class Main {
     private static Scanner sc = new Scanner(System.in);
+    private static double input = 0;
+    private static double rate = 0;
+    private static int years = 0;
 
     public static void main(String[] args) {
-        System.out.println("Input your savings :) (double) ");
-        double input = sc.nextDouble();
-        System.out.println("input the rate ! (in %) ");
-        double rate = sc.nextDouble();
-        System.out.println(" Input, for how years you want to save money  (int) ");
-        int years = sc.nextInt();
+        try {
+            System.out.println("Input your savings :) (double) ");
+             input = sc.nextDouble();
+            System.out.println("input the rate ! (in %) ");
+             rate = sc.nextDouble();
+            System.out.println(" Input, for how years you want to save money  (int) ");
+             years = sc.nextInt();
+        } catch (Exception e) {
+            System.err.println("Wrong or mismatched input ! ");
+            System.exit(-9000);
+        }
         rate = 1 + rate / 100;
         System.out.println("Your money after " + years + " years  have raised to " + save(input, rate, years) + "€");
     }
