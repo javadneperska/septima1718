e3e3859d3727eff78d1fcdcc540cb4bd9fc93827
kiramtas99 <36226103+kiramtas99@users.noreply.github.com>
2019-03-06 08:54:48 +0100

    Add files via upload

---
+++ Maturita/22/ptrojuholnik_satmarik.java
@@ -0,0 +1,33 @@
+import static java.awt.PageAttributes.MediaType.C;
+import java.io.*;
+import java.util.Scanner;
+
+public class ptrojuholnik_satmarik {
+        public static void main(String[] args) throws IOException {
+            File file = new File("C:\\Users\\Žiak\\Documents\\ptrojuholnik.txt");
+            FileWriter fw = new FileWriter(file);
+            BufferedWriter bw = new BufferedWriter(fw);
+
+            Scanner scn=new Scanner(System.in);
+            System.out.print("Zadajte počet * = ");
+            int n=scn.nextInt();
+
+            for (int i = 1; i <=n; i++) {
+                //_________________________________________
+                for (int j = n-i; j >=1; j--) {
+                    System.out.print(" ");
+                }
+                //_________________________________________
+                for (int k = 1; k <=i; k++) {
+                    System.out.print("*");
+                    bw.write("*");
+                }
+                //_________________________________________
+                System.out.println("");
+                bw.write("\n");
+            }
+
+            bw.close();
+        }
+
+    }
