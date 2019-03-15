167bd4b4f4993cf3008b15b3dad90a7a7851e718
dadadcko <dano.slanina@gmail.com>
2019-03-15 15:32:47 +0100

    mm ta co uz, not final yet

---
+++ Homework/hw3/src/sk/dneperska/daniel/slanina/Main.java
@@ -0,0 +1,8 @@
+package sk.dneperska.daniel.slanina;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Iban firstCustomer = new Iban("SK", "SLSP", 1234567890);
+    }
+}
