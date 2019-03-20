fbf82de59972414e900f48e78e594a9e2513bd71
kiramtas99 <36226103+kiramtas99@users.noreply.github.com>
2019-03-20 09:40:28 +0100

    Update MuzikJakub.java

--- Maturita/23/MuzikJakub/src/muzikjakub/MuzikJakub.java
+++ Maturita/23/MuzikJakub/src/muzikjakub/MuzikJakub.java
@@ -1,63 +1,100 @@
-
-package muzikjakub;
-
 import java.util.Scanner;
 
+public class iban_satmarik {
 
-public class MuzikJakub {
+    public static void main (String args[]){
 
+            String kod_uctu = null; //cez scanner
+            String kod_banky= null;
+            String kod_krajiny = "2820";
+            String predcislie = null;
+            //------------------------------------------------------
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Číslo účtu : (napr. : 1234567890) ");
+            kod_uctu = sc.next();
 
-    public static void main(String[] args) {
-     
-   String Predcislie = null;     
-   String Cislo_uctu = null;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Zadajte kód banky:");
-        int Kod_banky= sc.nextInt();
-        System.out.println("Zadajte predčíslie:");
-        Predcislie= sc.next();
-        if(Predcislie.length() <= 5){
-            Predcislie="000000";
-            System.out.println("Neplatné prtečíslie, automaticky zadané: 000000");
+            Scanner sc2 = new Scanner(System.in);
+            System.out.println("Kód banky (napr. : 0900) : ");
+            kod_banky = sc2.next();
+
+            Scanner sc3 = new Scanner(System.in);
+            System.out.println("Kód krajiny (napr. : SK = 2820) : ");
+            kod_krajiny = sc3.next();
+
+            Scanner sc4 = new Scanner(System.in);
+            System.out.println("Predčíslie (6 číslic) : (napr. 000000) : ");
+            predcislie = sc4.next();
+
+            //======================================================
+            if(kod_uctu.length() > 10){
+                System.out.println("Chybné číslo účtu!");
+                System.exit(1);
+            }
+            //------------------------------------------------------
+            if(kod_uctu.length() < 10){
+                StringBuilder builder = new StringBuilder(kod_uctu);
+                for (int i = kod_uctu.length(); i < 10; i++) {
+                    kod_uctu = "0"+kod_uctu; //dodá potrebný počet núl, aby číslo účtu malo 10 znakov
+                }
+            }
+            //======================================================
+                if(kod_banky.length() > 4){
+                   System.out.println("Chybný kód banky!");
+                   System.exit(1);
+              }
+            //------------------------------------------------------
+             if(kod_banky.length() < 4){
+                   System.out.println("Chybný kód banky!");
+                   System.exit(1);
+              }
+            //=====================================================
+            if(kod_krajiny.length() > 4){
+                    System.out.println("Chybný kód krajiny!");
+                    System.exit(1);
+             }
+            //------------------------------------------------------
+            if(kod_krajiny.length() < 4){
+                    System.out.println("Chybný kód krajiny!");
+                    System.exit(1);
+             }
+
+            //======================================================
+        if(predcislie.length() > 6){
+            System.out.println("Chybné predčíslie!");
+            System.exit(1);
         }
-        System.out.println("Zadajte kód krajiny:");
-        int Kod_krajiny= sc.nextInt();
-        System.out.println("Zadajte číslo účtu:");
-        Cislo_uctu = sc.next();
-       if(Cislo_uctu.length() > 10){
-           System.out.println("Chybné číslo účtu");
-           System.exit(0);
-
+        //------------------------------------------------------
+        if(predcislie.length() < 6){
+            System.out.println("Chybné predčíslie!");
+            System.exit(1);
         }
-        if(Cislo_uctu.length() < 10){
-            StringBuilder builder = new StringBuilder(Cislo_uctu);
-            for (int i = Cislo_uctu.length(); i < 10; i++) {
-               Cislo_uctu = "0"+Cislo_uctu;
+
+        //======================================================
+
+            System.out.println("Kód účtu :" + kod_uctu);
+            String Bban = kod_banky+"000000"+kod_uctu+kod_krajiny+"00";
+
+            int c_num = 98-(mod(Bban,97));
+            String num = Integer.toString(c_num); //98
+            if(c_num<10){
+                num = "0"+num;
             }
 
+            System.out.println("Kontrolný kód :" +num);
+            String IBAN = "SK"+num+kod_banky+predcislie+kod_uctu;
+            System.out.println("IBAN : " +IBAN);
         }
-        System.out.println(Cislo_uctu);
-        String Bban = Kod_banky+Predcislie+Cislo_uctu+Kod_krajiny+"00";
-        int Kontrolne_cislo = 98-(mod(Bban,97));
-        String number = Integer.toString(Kontrolne_cislo);
-        if(Kontrolne_cislo<10){
-            number = "0"+number;
+        static int mod(String num, int a)
+        {
+            int res = 0;
+
+            for (int i = 0; i < num.length(); i++)
+                res = (res * 10 + (int)num.charAt(i) - '0') % a;
+
+            return res;
         }
-        System.out.println(number);
-        String IBAN = "SK"+number+Kod_banky+Predcislie+Cislo_uctu;
-        System.out.println(IBAN);
     }
-    static int mod(String num, int a)
-    {
 
 
-        int res = 0;
 
 
-        for (int i = 0; i < num.length(); i++)
-            res = (res * 10 + (int)num.charAt(i)
-                    - '0') % a;
-
-        return res;
-    }
-}
