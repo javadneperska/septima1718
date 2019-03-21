0be4959a2b8c197e5765db312a5d13e193b16769
dadadcko <dano.slanina@gmail.com>
2019-03-21 15:29:53 +0100

    BUGFIX :D

--- Maturita/23/HunorRadovan/src/Main.java
+++ Maturita/23/HunorRadovan/src/Main.java
@@ -1,3 +1,5 @@
+import org.jetbrains.annotations.NotNull;
+
 import java.math.BigInteger;
 import java.util.Scanner;
 
@@ -11,23 +13,25 @@ public class Main {
         String KB = sc.next();
         System.out.println("Insert Predcislie učtu:");
         String PU = sc.next();
-        System.out.println(IBAN(CU,KB,PU));
+        System.out.println(IBAN(CU, KB, PU));
     }
-    public static String IBAN(String CU, String KB,String PU){
-        String IBANumber = "";
-        if(CU.length() < 10){
-            for(int x = 0;CU.length()<10;x++){
-                CU = "0"+CU;
+
+    @NotNull
+    private static String IBAN(String CU, String KB, String PU) {
+        if (CU.length() < 10) {
+            StringBuilder CUBuilder = new StringBuilder(CU);
+            while (CUBuilder.length() < 10) {
+                CUBuilder.insert(0, "0");
             }
+            CU = CUBuilder.toString();
         }
-        String MV = KB+PU+CU+"282000";
+        String MV = KB + PU + CU + "282000";
         BigInteger bi1, bi2, bi3;
         bi1 = new BigInteger(MV);
-
         bi2 = new BigInteger("97");
         bi3 = bi1.mod(bi2);
-        int checksum = 98 - bi3.intValue();
-        IBANumber = "SK" + checksum + KB + PU + CU;
-        return IBANumber;
+        String checksum = String.valueOf(98 - bi3.intValue());
+        if (Integer.valueOf(checksum) < 10) checksum = "0" + checksum;
+        return "SK" + checksum + KB + PU + CU;
     }
 }
