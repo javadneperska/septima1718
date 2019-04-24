2512e2379e0e30c1a0de7c3851313b310d878ed9
kiramtas99 <36226103+kiramtas99@users.noreply.github.com>
2019-04-24 09:20:46 +0200

    (☭ ͜ʖ ☭) - update

--- Maturita/24/SlaninaDaniel/src/sk/dneperska/septima1718/Main.java
+++ Maturita/24/SlaninaDaniel/src/sk/dneperska/septima1718/Main.java
@@ -6,6 +6,7 @@ public class Main {
     private static Scanner sc = new Scanner(System.in);
     private static int x, y, z;
     private static StringBuilder output = new StringBuilder();
+    private static int counter;
 
     public static void main(String[] args) {
 
@@ -28,24 +29,70 @@ public class Main {
             System.err.println("Wrong input ! ");
             System.exit(2);
         }
+
+        int[] array;
         if (x <= y) {
             for (int i = x; i <= y; i++) {
                 if (i % z == 0) {
-                    output.append(",").append(i);
+                    counter++;
+                }
+            }
+            array = new int[counter];
+            int a = 0;
+            for (int i = x; i <= y; i++) {
+                if (i % z == 0) {
+                    array[a] = i;
+                    a++;
+                }
+            }
+        } else {
+            for (int i = y; i <= x; i++) {
+                if (i % z == 0) {
+                    counter++;
+                }
+            }
+            array = new int[counter];
+            int a = 0;
+            for (int i = y; i <= x; i++) {
+                if (i % z == 0) {
+                    array[a] = i;
+                    a++;
                 }
             }
         }
-        else {
-            for (int i = y; i <= x; i++) {
-                if (i % z == 0) {
-                    output.append(",").append(i);
-                }
+
+        for (int number : array) {
+            if (number >= 0) {
+                int factorial = factorial(number);
+                int primeNumber = isPrimeNumber(factorial);
+                if (primeNumber > 0) output.append(", ").append(primeNumber);
+
             }
         }
 
         output.deleteCharAt(0);
-        System.out.println("Numbers giving integer after division: " +output);
+        System.out.println("Numbers giving integer after division ( in factorial value) : " + output);
 
+    }
+
+    private static int isPrimeNumber(int PrimeNumber) {
+        int i = 2;
+        while (i <= PrimeNumber / 2) {
+            // condition for nonprime number
+            if (PrimeNumber % i == 0) {
+                return 0;
+            }
+
+            ++i;
+        }
+        return PrimeNumber;
+    }
+
+    private static int factorial(int number) {
+        if (number == 0) return 1;
+        else {
+            return number * factorial(number - 1);
+        }
     }
 
 
